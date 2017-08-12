6a276e274a0b3105cac9560016c4dc66e8265fd7
crystalDf <chendong333@gmail.com>
2017-08-12 22:43:53 +0800

    Chapter 03

--- src/ch03/sec04/LengthComparator.java
+++ src/ch03/sec04/LengthComparator.java
@@ -6,8 +6,22 @@ public class LengthComparator {
 
     public static void main(String[] args) {
 
+//        1st method
+//        Comparator<String> comparator =
+//                (String first, String second) -> first.length() - second.length();
+
         Comparator<String> comparator =
-                (String first, String second) -> first.length() - second.length();
+                (String first, String second) -> {
+                    int difference = first.length() - second.length();
+
+                    if (difference < 0) {
+                        return -1;
+                    } else if (difference > 0) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                };
 
 //        Comparator<String> comparator =
 //                Comparator.comparingInt(String::length);
