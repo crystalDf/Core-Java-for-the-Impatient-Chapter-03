880f9afe91890f147cb9cd3a13f9ac365b4cdfd4
crystalDf <chendong333@gmail.com>
2017-08-16 23:20:05 +0800

    Chapter 03

---
+++ src/ch03/sec07/ThisTest.java
@@ -0,0 +1,23 @@
+package ch03.sec07;
+
+public class ThisTest {
+
+    public static void main(String[] args) {
+
+        new ThisTest().doWork();
+    }
+
+    public void doWork() {
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(this.toString());
+            }
+        };
+
+//        Runnable runnable = () -> System.out.println(this.toString());
+
+        new Thread(runnable).start();
+    }
+}
